ec1886545fe75ad1fbb2cb67c94d3def2b2eec38
Paul Carmichael <paulcarmo@gmail.com>
2018-12-30 23:21:07 +0000

    bertlv parsing support

--- bertlv.go
+++ bertlv.go
@@ -2,35 +2,39 @@ package cryptop
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"log"
 	"strconv"
+	"strings"
 	"sync"
 )
 
 // BERTLVTag struct represents an individual tag
 type BERTLVTag struct {
-	Tag       string
-	Length    string // used for parsing
-	MinLength string // used for tag definitions
-	MaxLength string // used for tag definitions
-	Value     string
-	Name      string
-	Format    string
+	Tag           string
+	Length        string
+	MinLength     string
+	MaxLength     string
+	Value         string
+	Name          string
+	Format        string
+	Valid         bool
+	InvalidReason string
 }
 
 // EMVDictionary is supported as a singleton containing all EMV tag info
 type EMVDictionary struct {
-	Tags []BERTLVTag
+	Tags map[string]BERTLVTag
 }
 
-var singleton *EMVDictionary
+var dictionary *EMVDictionary
 var once sync.Once
 
 // GetDictionary returns a populated EMVDictionary
 func GetDictionary() *EMVDictionary {
 	once.Do(func() {
-		singleton = &EMVDictionary{}
+		dictionary = &EMVDictionary{}
 
 		// open the emvtags.json file
 		bytes, err := ioutil.ReadFile("../src/github.com/paulcarmichael/cryptop/emvtags.json")
@@ -40,38 +44,37 @@ func GetDictionary() *EMVDictionary {
 		}
 
 		// read the file into the EMVDictionary singleton
-		err = json.Unmarshal(bytes, singleton)
+		err = json.Unmarshal(bytes, dictionary)
 
 		if err != nil {
 			log.Fatal("Unmarshal: ", err)
 		}
 
-		// check the contents
-		for _, tag := range singleton.Tags {
-			log.Println("Read EMV tag", tag.Tag)
-		}
-
-		log.Println("EMV dictionary contains", len(singleton.Tags), "tags")
+		log.Println("EMV dictionary contains", len(dictionary.Tags), "tags")
 	})
 
-	return singleton
+	return dictionary
 }
 
-// ParseBERTLV is given a string of BERTLV data and returns the parsed tags
-func ParseBERTLV(data string) ([]BERTLVTag, error) {
-	data = "9F1008AAAAAAAAAAAAAAAA9004BBCC"
+// BERTLVParser struct to be populated by the caller
+type BERTLVParser struct {
+	Request string
+}
 
-	var tags []BERTLVTag
-
-	// pack the data
-	data, err := Pack(data)
+// Parse is given a string of BERTLV data and returns the parsed tags
+func (p BERTLVParser) Parse() (string, error) {
+	// pack the request
+	data, err := Pack(p.Request)
 
 	if err != nil {
-		return tags, err
+		return "", err
 	}
 
 	// parse
+	var tags []BERTLVTag
+
 	for len(data) != 0 {
+
 		var tag BERTLVTag
 
 		// parse the tag, which can be multiple bytes long, identified by the presence of bit 4
@@ -89,14 +92,14 @@ func ParseBERTLV(data string) ([]BERTLVTag, error) {
 		tag.Tag, err = Expand([]byte(tag.Tag))
 
 		if err != nil {
-			return tags, err
+			return "", err
 		}
 
 		// parse the length, which is one byte
 		tag.Length, err = Expand([]byte(data[:1]))
 
 		if err != nil {
-			return tags, err
+			return "", err
 		}
 
 		data = data[1:]
@@ -105,25 +108,76 @@ func ParseBERTLV(data string) ([]BERTLVTag, error) {
 		valueLength, err := strconv.ParseInt(tag.Length, 16, 16)
 
 		if err != nil {
-			return tags, err
+			return "", err
+		}
+
+		// sanity check the given length against the data length
+		if valueLength > int64(len(data)) {
+			var b strings.Builder
+
+			b.WriteString("ParseBERTLV: Tag ")
+			b.WriteString(tag.Tag)
+			b.WriteString(" has length ")
+			b.WriteString(tag.Length)
+			b.WriteString(" but there are only ")
+			b.WriteString(strconv.Itoa(len(data)))
+			b.WriteString(" bytes remaining")
+
+			return "", errors.New(b.String())
 		}
 
 		tag.Value, err = Expand([]byte(data[:valueLength]))
 
 		if err != nil {
-			return tags, err
-		}
-
-		// sanity check the given length against the data length
-		if valueLength > int64(len(data)) {
-			// exit!
+			return "", err
 		}
 
 		data = data[valueLength:]
+
+		// lookup the tag details in the EMVDictionary
+		if dictionaryTag, found := dictionary.Tags[tag.Tag]; found {
+			if dictionaryTag.MinLength <= tag.Length &&
+				dictionaryTag.MaxLength >= tag.Length {
+				tag.Valid = true
+			} else {
+				var b strings.Builder
+
+				b.WriteString("Tag ")
+				b.WriteString(tag.Tag)
+				b.WriteString(" has length ")
+				b.WriteString(tag.Length)
+				b.WriteString(" but the EMV 4.1 specification states it should be minimum ")
+				b.WriteString(dictionaryTag.MinLength)
+				b.WriteString(" to maximum ")
+				b.WriteString(dictionaryTag.MaxLength)
+
+				tag.InvalidReason = b.String()
+			}
+
+			tag.Name = dictionaryTag.Name
+			tag.Format = dictionaryTag.Format
+			tag.MinLength = dictionaryTag.MinLength
+			tag.MaxLength = dictionaryTag.MaxLength
+		} else {
+			var b strings.Builder
+
+			b.WriteString("Tag ")
+			b.WriteString(tag.Tag)
+			b.WriteString(" is unknown to the EMV 4.1 specification")
+
+			tag.InvalidReason = b.String()
+		}
 
 		// add the tag to our slice of parsed tags
 		tags = append(tags, tag)
 	}
 
-	return tags, nil
+	// convert the slice of parsed tags into json
+	result, err := json.Marshal(tags)
+
+	if err != nil {
+		return "Failed to convert parsed tags to json", nil
+	}
+
+	return string(result), nil
 }
